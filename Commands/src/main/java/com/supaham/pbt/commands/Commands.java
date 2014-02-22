8cdd8f01f7f1fca78e98d5768d9b8962bcfcf210
Ali Moghnieh <SupaHam.me@Gmail.com>
2014-02-22 02:22:57 +0000

    Commands actually work now.

--- Commands/src/main/java/com/supaham/pbt/commands/Commands.java
+++ Commands/src/main/java/com/supaham/pbt/commands/Commands.java
@@ -1,18 +1,23 @@
 package com.supaham.pbt.commands;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import pluginbase.bukkit.command.BukkitCommandProvider;
 import pluginbase.bukkit.messaging.BukkitMessager;
+import pluginbase.bukkit.minecraft.BukkitTools;
 import pluginbase.bukkit.permission.BukkitPermFactory;
 import pluginbase.command.Command;
+import pluginbase.command.CommandException;
 import pluginbase.command.CommandHandler;
 import pluginbase.command.CommandProvider;
+import pluginbase.command.CommandUsageException;
 import pluginbase.command.QueuedCommand;
 import pluginbase.logging.LoggablePlugin;
 import pluginbase.logging.PluginLogger;
 import pluginbase.messages.messaging.Messager;
 import pluginbase.messages.messaging.Messaging;
+import pluginbase.minecraft.BasePlayer;
 import pluginbase.permission.PermFactory;
 
 import java.io.File;
@@ -26,7 +31,7 @@ public class Commands extends JavaPlugin implements LoggablePlugin, Messaging, C
 
     static {
         PermFactory.registerPermissionFactory(BukkitPermFactory.class);
-        PermFactory.registerPermissionName(Commands.class, "testcmds");
+        PermFactory.registerPermissionName(Commands.class, "test");
     }
 
     @Override
@@ -34,6 +39,31 @@ public class Commands extends JavaPlugin implements LoggablePlugin, Messaging, C
         logger = PluginLogger.getLogger(this);
         commandProvider = BukkitCommandProvider.getBukkitCommandProvider(this, "test");
         getCommandHandler().registerCommand(AwesomeCommand.class);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+
+        if (!isEnabled()) {
+            sender.sendMessage("This plugin is Disabled!");
+            return true;
+        }
+        
+        final BasePlayer wrappedSender = BukkitTools.wrapSender(sender);
+        String[] allArgs = new String[args.length + 1];
+        allArgs[0] = command.getName();
+        System.arraycopy(args, 0, allArgs, 1, args.length);
+        try {
+            return getCommandHandler().locateAndRunCommand(wrappedSender, allArgs);
+        } catch (CommandException e) {
+            e.sendException(getMessager(), wrappedSender);
+            if (e instanceof CommandUsageException) {
+                for (final String usageString : ((CommandUsageException) e).getUsage()) {
+                    wrappedSender.sendMessage(usageString);
+                }
+            }
+        }
+        return true;
     }
 
     @Override
